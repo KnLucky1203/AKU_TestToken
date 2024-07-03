f1de81339e1ea8e183be88e98de95cd29b3111ca
GloryDream413 <glorydream413@gmail.com>
2024-07-03 18:28:52 +0900

    Project updated.

--- DeployProject/migrations/2_contract_deployment.js
+++ DeployProject/migrations/2_contract_deployment.js
@@ -1,30 +1,38 @@
-const WMATIC = artifacts.require("WMATIC");
-const WBNB = artifacts.require("WBNB");
-const WETH = artifacts.require("WETH");
-const LINK = artifacts.require("LINK");
+const ARB = artifacts.require("WMATIC");
+const AVAX = artifacts.require("WBNB");
+const DAI = artifacts.require("WETH");
+const LTC = artifacts.require("WETH");
+const MATIC = artifacts.require("LINK");
+const SOL = artifacts.require("TRX");
 const TRX = artifacts.require("TRX");
-const A = artifacts.require("A");
 const USDT = artifacts.require("USDT");
+const WBNB = artifacts.require("WBNB");
+const WBTC = artifacts.require("WBNB");
+const WETH = artifacts.require("WETH");
+const XMR = artifacts.require("XMR");
 
 module.exports = async function (deployer) {
-  await deployer.deploy(WMATIC);
-  await WMATIC.deployed();
+  await deployer.deploy(ARB);
+  await ARB.deployed();
 
-  await deployer.deploy(WBNB);
-  await WBNB.deployed();
+  await deployer.deploy(AVAX);
+  await AVAX.deployed();
 
-  await deployer.deploy(WETH);
-  await WETH.deployed();
+  await deployer.deploy(DAI);
+  await DAI.deployed();
 
-  await deployer.deploy(LINK);
-  await LINK.deployed();
+  await deployer.deploy(LTC);
+  await LTC.deployed();
+
+  await deployer.deploy(MATIC);
+  await MATIC.deployed();
+
+  await deployer.deploy(SOL);
+  await SOL.deployed();
 
   await deployer.deploy(TRX);
   await TRX.deployed();
 
-  await deployer.deploy(A);
-  await A.deployed();
-
-  await deployer.deploy(USDT);
-  await USDT.deployed();
+  await deployer.deploy(TRX);
+  await TRX.deployed();
 };
